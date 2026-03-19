ff4a74a3589e37a1b5d558132064179043efb8d4
Hajar <hajardiab@outlook.fr>
2026-03-19 17:11:15 +0100

    API sans commentaires

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,5 +1,6 @@
 import { bookDB } from "../database/book.database.js";
 
+
 const createBook = async (req, res) => {
     try{
         const {title, autor, resume, published_at, cover, genre, verified} = req.body
@@ -12,7 +13,7 @@ const createBook = async (req, res) => {
     }
 }
 
-// Fonction pour récupérer tous les books
+
 const readBooks = async (req, res) => {
   try {
     const booksResponse = await bookDB.readBooks();
@@ -26,6 +27,7 @@ const readBooks = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
 
 const readOneBook = async (req, res) => {
   try {
@@ -55,8 +57,46 @@ const readOneBook = async (req, res) => {
     }
 }
 
+const updateBook = async (req, res) => {
+    try{
+        const {title, autor, resume, published_at, cover, genre, verified} = req.body
+        const id_books=req.params.id_books
+        const response = await bookDB.updateBook(title, autor, resume, published_at, cover, genre, verified, id_books)
+        if(response.error){return res.status(500).json({message: response.error})}
+        return res.status(201).json({message: `Le livre ${title} a été modifié avec succès`})
+    } 
+    catch(error){console.error("Controller error",error)
+        return res.status(500).json({message: error.message})
+    }
+}
+
+const deleteBook = async (req, res) => {
+  try {
+    const id_books = req.params.id_books;
+    const response = await bookDB.deleteOneBook(id_books);
+    if (response.error) {
+      return res.status(500).json({ message: response.error });
+    }
+    if (!response.result || response.result.affectedRows === 0) {
+      return res.status(404).json({ message: "Livre non trouvé" });
+    }
+    return res
+      .status(200)
+      .json({
+        message: "Livre supprimé avec succès",
+        id_books: Number(id_books),
+      });
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 export const BookController = {
     readOneBook,
     readBooks,
-    createBook
+    createBook,
+    updateBook,
+    deleteBook
 }
+
